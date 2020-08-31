3f9583fc2c71ef814bb1c409fcc404be2d02e4f9
FreakyCoder <kurayogun@gmail.com>
2020-08-31 14:03:22 +0300

    New eslintrc, babelrc and tsconfig.json files are here

--- metro.config.js
+++ metro.config.js
@@ -11,6 +11,7 @@ module.exports = {
       transform: {
         experimentalImportSupport: false,
         inlineRequires: false,
+        resetCache: true,
       },
     }),
   },
--- src/services/navigation/index.tsx
+++ src/services/navigation/index.tsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-dynamic-vector-icons';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-dynamic-vector-icons";
 
 // ? Screens
-import HomeScreen from '@screens/home/HomeScreen';
-import SearchScreen from '@screens/search/SearchScreen';
-import DetailScreen from '@screens/detail/DetailScreen';
+import HomeScreen from "@screens/home/HomeScreen";
+import SearchScreen from "@screens/search/SearchScreen";
+import DetailScreen from "@screens/detail/DetailScreen";
 
 // ? If you want to use stack or tab or both
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-const Navigation = (props) => {
+const Navigation = (props: any) => {
   const renderTabNavigation = () => {
     return (
       <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string = "";
 
-            if (route.name === 'Home') {
-              iconName = focused ? 'ios-heart' : 'heart-outline';
-            } else if (route.name === 'Search') {
-              iconName = focused ? 'ios-search' : 'ios-search';
+            if (route.name === "Home") {
+              iconName = focused ? "ios-heart" : "heart-outline";
+            } else if (route.name === "Search") {
+              iconName = focused ? "ios-search" : "ios-search";
             }
 
             // You can return any component that you like here!
@@ -34,9 +34,10 @@ const Navigation = (props) => {
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
       </Tab.Navigator>
@@ -48,7 +49,8 @@ const Navigation = (props) => {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-        }}>
+        }}
+      >
         <Stack.Screen name="Home" component={renderTabNavigation} />
         <Stack.Screen name="Detail">
           {(props) => <DetailScreen {...props} />}
