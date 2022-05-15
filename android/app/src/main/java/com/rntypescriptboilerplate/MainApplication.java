07284cb025124c442fb56bb59630c8e6be6f8905
FreakyCoder <kurayogun@gmail.com>
2022-05-15 18:54:01 +0300

    fix: android splash screen

--- android/app/src/main/java/com/rntypescriptboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rntypescriptboilerplate/MainApplication.java
@@ -13,7 +13,7 @@ import com.rntypescriptboilerplate.newarchitecture.MainApplicationReactNativeHos
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage; 
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
